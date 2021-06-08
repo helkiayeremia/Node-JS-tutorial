dd06df8a0af49545c21894b7f40926bdf08671a6
helkiayeremia <helkia77@gmail.com>
2021-06-08 19:55:52 +0700

    191 Logging out Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 router.post('/forgotPassword', authController.forgotPassowrd);
 router.patch('/resetPassword/:token', authController.resetPassowrd);
 
