49d03bb1d0075291de9385040039ddc2043af098
helkiayeremia <helkia77@gmail.com>
2021-06-09 14:17:00 +0700

    194 Updating User Data

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8906,7 +8906,7 @@ var _login = require("./login");
 
 // DOM ElEMENTS
 var mapBox = document.getElementById('map');
-var loginForm = document.querySelector('.form');
+var loginForm = document.querySelector('.form--login');
 var logOutBtn = document.querySelector('.nav__el--logout'); // VALUES
 // DELEGATIONS
 
@@ -8953,7 +8953,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44881" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39127" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,7 @@ import { login, logout } from './login';
 
 // DOM ElEMENTS
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 
 // VALUES
