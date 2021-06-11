fc8eaf711ad679f58e602cd85b4b18670a8fdbda
helkiayeremia <helkia77@gmail.com>
2021-06-11 14:14:24 +0700

    202 Adding Image Uploads to Form

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel');
+const sharp = require('sharp');
 const multer = require('multer');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
@@ -13,6 +14,8 @@ const multerStorage = multer.diskStorage({
     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
   }
 });
+
+// const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -29,6 +32,19 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
+// exports.resizeUserPhoto = (req, res, next) => {
+//   if (!req.file) return next();
+
+//   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+//   sharp(req.file.buffer)
+//     .resize(500, 500)
+//     .toFormat('jpeg')
+//     .toFile(`pulic/img/users/${req.file.filename}`);
+
+//   next();
+// };
+
 const filterObj = (obj, ...allowedField) => {
   const newObj = {};
   Object.keys(obj).forEach(el => {
@@ -38,9 +54,6 @@ const filterObj = (obj, ...allowedField) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
-
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -53,6 +66,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 4) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
